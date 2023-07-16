1039833ff22dc8ae9d05e47517f6993d20a1db19
Ihor Lukianov <lukianov.igor@hotmail.com>
2023-07-16 14:10:04 +0700

    added extraction of subreddits attributes

--- src/main.py
+++ src/main.py
@@ -1,18 +1,34 @@
-import praw
+from praw import Reddit
+from praw.models import Subreddit
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
-reddit = praw.Reddit(client_id=config['CLIENT_ID'],
-                     client_secret=config['CLIENT_SECRET'],
-                     redirect_url='http://localhost:a8080',
-                     user_agent=config['USER_AGENT'])
+reddit = Reddit(client_id=config['CLIENT_ID'],
+                client_secret=config['CLIENT_SECRET'],
+                redirect_url='http://localhost:a8080',
+                user_agent=config['USER_AGENT'])
 
-subredit_name = 'Switzerland'
-posts = reddit.subreddit(subredit_name).hot(limit=10)
+subreddit_names = ['Thailand', 'malaysia', 'japan',]
+data = []
 
-title = {subredit_name + '_' + str(i+1): post.title for i, post in enumerate(posts)}
+for subreddit_name in subreddit_names:
+    subreddit = Subreddit(reddit=reddit, display_name=subreddit_name)
     
-print(title)
+    subreddit_dict = {'full_name': subreddit.fullname,
+                      'display_name': subreddit.display_name,
+                      'title': subreddit.title,
+                      'public_description': subreddit.public_description,
+                      'created_utc': int(subreddit.created_utc),
+                      'subscribers': subreddit.subscribers,
+                      'lang': subreddit.lang,
+                      'subreddit_type': subreddit.lang,
+                      'over18': subreddit.over18}
+    
+    data.append(subreddit_dict)
+  
+print(data)  
+# posts = reddit.subreddit(subredit_name).hot(limit=10)
 
+# title = {subredit_name + '_' + str(i+1): post.title for i, post in enumerate(posts)}
 
