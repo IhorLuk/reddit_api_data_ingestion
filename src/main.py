704ecd033d2c06ad1e59eafb7973581f97397c7a
Ihor Lukianov <lukianov.igor@hotmail.com>
2023-08-12 14:21:17 +0700

    separate class for RedditLoader instance, simplified code

--- src/main.py
+++ src/main.py
@@ -2,15 +2,9 @@ from praw import Reddit
 from praw.models import Subreddit, Submission
 import os
 from dotenv import load_dotenv
+from reddit import RedditLoader
 from datetime import datetime, date, timedelta
 import pandas as pd
-
-# connect to reddit API service
-load_dotenv()
-reddit = Reddit(client_id=os.getenv('CLIENT_ID'),
-                client_secret=os.getenv('CLIENT_SECRET'),
-                redirect_url='http://localhost:a8080',
-                user_agent=os.getenv('USER_AGENT'))
 
 # make time limits - we need only yesterday's posts
 START_DATE = date.today() - timedelta(days = 1)
@@ -20,64 +14,13 @@ END_DATE = date.today()
 with open('../content/subreddits.txt') as f:
     content = f.read()
     subreddit_names = content.split('\n')
-
+            
 # prepare list for a future dataframe
 data = []
+RedditLoader = RedditLoader()
 
-# loop on getting all the data
 for subreddit_name in subreddit_names:
-    subreddit = Subreddit(reddit=reddit, display_name=subreddit_name)
-    
-    for submission_id in subreddit.new(limit=100):
-        submission = Submission(reddit=reddit, id=submission_id)
-        
-        # we don't want to see data for stickied (or pinned) submissions
-        if not submission.stickied:
-            # convert timestamp to a datetime format
-            created_time = datetime.fromtimestamp(int(submission.created_utc))
-            # check if submission was created yesterday
-            if created_time.date() == START_DATE:
-                
-                submission.comments.replace_more(limit=None)
-                comment_queue = submission.comments[:]  # Seed with top-level
-                while comment_queue:
-                    comment = comment_queue.pop(0)
-                    
-                    # not all comments have available author name
-                    try:
-                        author_name = comment.author.name
-                    except AttributeError:
-                        author_name = None
-                        
-                    # prepare row for dataframe
-                    row = {
-                        'comment_id': comment.id,
-                        'comment_author': author_name,
-                        'comment_created_time': datetime.fromtimestamp(int(comment.created_utc)),
-                        'commentnum_upvotes': comment.score,
-                        'comment_text': comment.body,
-                        'submission_id': submission.id,
-                        'submission_author': submission.author.name,
-                        'submission_title': submission.title,
-                        'submission_created_time': created_time,
-                        'submission_name': submission.name,
-                        'submission_num_comments': submission.num_comments,
-                        'submission_num_upvotes': submission.score,
-                        'submission_text': submission.selftext,
-                        'submission_url': submission.url,
-                        'subreddit_full_name': subreddit.fullname,
-                        'subreddit_display_name': subreddit.display_name,
-                        'subreddit_title': subreddit.title,
-                        'subreddit_public_description': subreddit.public_description,
-                        'subreddit_created_utc': datetime.fromtimestamp(int(subreddit.created_utc)),
-                        'subreddit_subscribers': subreddit.subscribers,
-                        'subreddit_lang': subreddit.lang,
-                        'subreddit_subreddit_type': subreddit.lang,
-                        'subreddit_over18': subreddit.over18
-                        }
-                    
-                    data.append(row)
-                    comment_queue.extend(comment.replies)
+    data.extend(RedditLoader.collect_comments(subreddit_name=subreddit_name, start_date=START_DATE))
   
 full_data = pd.DataFrame(data)
 # save to parquet file in a format 'reddit-comments-YYYY-MM-DD'
---
+++ src/reddit.py
@@ -0,0 +1,95 @@
+from praw import Reddit
+from praw.models import Subreddit, Submission
+import os
+from dotenv import load_dotenv
+from datetime import datetime, date, timedelta
+
+class RedditLoader():
+    def __init__(self) -> None:
+        # connect to reddit API service
+        load_dotenv()
+        self.reddit = Reddit(client_id=os.getenv('CLIENT_ID'),
+                        client_secret=os.getenv('CLIENT_SECRET'),
+                        redirect_url='http://localhost:a8080',
+                        user_agent=os.getenv('USER_AGENT'))
+        
+
+            
+    def get_subreddit(self, subreddit_name):
+        """Creates subreddit instance to get submissions from it
+
+        Args:
+            subreddit_name (str): name of subreddit
+        """
+        self.subreddit = Subreddit(reddit=self.reddit, display_name=subreddit_name)
+        
+    def get_submission(self, submission_id):
+        """Creates a submission instance, used for getting comments.
+
+        Args:
+            submission_id (str): the id of submission
+        """
+        self.submission = Submission(reddit=self.reddit, id=submission_id)
+        
+    def get_comments(self, start_date):
+        submission_data = []
+        if not self.submission.stickied:
+            # convert timestamp to a datetime format
+            created_time = datetime.fromtimestamp(int(self.submission.created_utc))
+            # check if submission was created yesterday
+            if created_time.date() == start_date:
+                
+                self.submission.comments.replace_more(limit=None)
+                comment_queue = self.submission.comments[:]  # Seed with top-level
+                while comment_queue:
+                    comment = comment_queue.pop(0)
+                    
+                    # not all comments have available author name
+                    try:
+                        author_name = comment.author.name
+                    except AttributeError:
+                        author_name = None
+                        
+                    # prepare row for dataframe
+                    row = {
+                        'comment_id': comment.id,
+                        'comment_author': author_name,
+                        'comment_created_time': datetime.fromtimestamp(int(comment.created_utc)),
+                        'commentnum_upvotes': comment.score,
+                        'comment_text': comment.body,
+                        'submission_id': self.submission.id,
+                        'submission_author': self.submission.author.name,
+                        'submission_title': self.submission.title,
+                        'submission_created_time': created_time,
+                        'submission_name': self.submission.name,
+                        'submission_num_comments': self.submission.num_comments,
+                        'submission_num_upvotes': self.submission.score,
+                        'submission_text': self.submission.selftext,
+                        'submission_url': self.submission.url,
+                        'subreddit_full_name': self.subreddit.fullname,
+                        'subreddit_display_name': self.subreddit.display_name,
+                        'subreddit_title': self.subreddit.title,
+                        'subreddit_public_description': self.subreddit.public_description,
+                        'subreddit_created_utc': datetime.fromtimestamp(int(self.subreddit.created_utc)),
+                        'subreddit_subscribers': self.subreddit.subscribers,
+                        'subreddit_lang': self.subreddit.lang,
+                        'subreddit_subreddit_type': self.subreddit.lang,
+                        'subreddit_over18': self.subreddit.over18
+                        }
+                    
+                    submission_data.append(row)
+                    comment_queue.extend(comment.replies)
+                    return submission_data
+                    
+    def collect_comments(self, subreddit_name, start_date):
+        subreddit_data = []
+        self.get_subreddit(subreddit_name)
+        
+        for submission_id in self.subreddit.new(limit=100):
+            self.get_submission(submission_id)
+            
+            new_data = self.get_comments(start_date)
+            
+            if new_data is not None:
+                subreddit_data.extend(new_data)
+        return subreddit_data
