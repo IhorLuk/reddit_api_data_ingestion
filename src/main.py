1b1153abe8a08be5e7c4a6bc7f22fb2a87e344e6
Ihor Lukianov <lukianov.igor@hotmail.com>
2023-08-12 17:48:28 +0700

    bugs fix, added documentation to the reddit class

--- src/main.py
+++ src/main.py
@@ -1,9 +1,5 @@
-from praw import Reddit
-from praw.models import Subreddit, Submission
-import os
-from dotenv import load_dotenv
 from reddit import RedditLoader
-from datetime import datetime, date, timedelta
+from datetime import date, timedelta
 import pandas as pd
 
 # make time limits - we need only yesterday's posts
@@ -24,4 +20,4 @@ for subreddit_name in subreddit_names:
   
 full_data = pd.DataFrame(data)
 # save to parquet file in a format 'reddit-comments-YYYY-MM-DD'
-full_data.to_parquet(f'reddit-comments-{START_DATE}')
+full_data.to_parquet(f'../content/reddit-comments-{START_DATE}')
--- src/reddit.py
+++ src/reddit.py
@@ -6,6 +6,8 @@ from datetime import datetime, date, timedelta
 
 class RedditLoader():
     def __init__(self) -> None:
+        """Loads credentials from environment and starts reddit client
+        """
         # connect to reddit API service
         load_dotenv()
         self.reddit = Reddit(client_id=os.getenv('CLIENT_ID'),
@@ -32,6 +34,14 @@ class RedditLoader():
         self.submission = Submission(reddit=self.reddit, id=submission_id)
         
     def get_comments(self, start_date):
+        """Collects all comments for a submission and returns list of dicts
+
+        Args:
+            start_date (date): date to collect comments for
+
+        Returns:
+            list: list of dicts, where each dict is a row for a comments of submission
+        """
         submission_data = []
         if not self.submission.stickied:
             # convert timestamp to a datetime format
@@ -76,20 +86,32 @@ class RedditLoader():
                         'subreddit_subreddit_type': self.subreddit.lang,
                         'subreddit_over18': self.subreddit.over18
                         }
-                    
                     submission_data.append(row)
                     comment_queue.extend(comment.replies)
-                    return submission_data
+        return submission_data
                     
     def collect_comments(self, subreddit_name, start_date):
+        """The main function, that collections all comments for selected date (ideally it should be yesterday) and returns list of dicts.
+
+        Args:
+            subreddit_name (str): the name of subreddit.
+            start_date (date): for which date to collect comments
+
+        Returns:
+            list: list of dictionaries, each dictionary is a row of data. Could be used to create a dataframe.
+        """
         subreddit_data = []
         self.get_subreddit(subreddit_name)
         
         for submission_id in self.subreddit.new(limit=100):
             self.get_submission(submission_id)
+            submission_time = datetime.fromtimestamp(int(self.submission.created_utc))
+            if submission_time.date() < start_date:
+                break
             
             new_data = self.get_comments(start_date)
             
             if new_data is not None:
                 subreddit_data.extend(new_data)
+            
         return subreddit_data
